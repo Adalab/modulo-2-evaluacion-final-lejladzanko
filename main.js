96d7ea27c502b2f6ea2303e99737ee8e111c901f
Lejla <lejladzanko@gmail.com>
2024-03-06 07:52:47 +0100

    reset favorites modified, styles modified

--- main.js
+++ main.js
@@ -3,9 +3,10 @@
 const animeAPIURL = "https://api.jikan.moe/v4/anime?q=";
 const searchInput = document.querySelector(".js-search-text");
 const searchButton = document.querySelector(".js-search");
-const resetButton = document.querySelector('.js-reset');
+const resetButton = document.querySelector(".js-reset");
 const seriesResultsContainer = document.querySelector(".js-series");
 const favoritesResultsContainer = document.querySelector(".js-favorites");
+const resetFavoritesButton = document.querySelector(".js-reset-favorites");
 
 let seriesList = [];
 let favoritesList = [];
@@ -26,16 +27,17 @@ function handleAddFavorite(event) {
     renderFavorites(favoritesList);
     localStorage.setItem("favorites", JSON.stringify(favoritesList));
     //añadir otro color de fondo y fuente al seleccionar series como favorita
-    event.currentTarget.classList.add('favorite');
+    event.currentTarget.classList.add("favorite");
   }
 }
 
-const renderSeries = (series) => {
+function renderSeries(series){
   let content = "";
   for (const seriesItem of series) {
+    const imageUrl = getImageUrl(seriesItem.images.jpg.large_image_url);
     content += `
         <div class="series-item" data-id="${seriesItem.mal_id}" id="${seriesItem.mal_id}">
-                <img src="${seriesItem.images.jpg.large_image_url}">
+        <img src="${imageUrl}">
                 <h3>${seriesItem.title}</h3>
             </div>
         `;
@@ -51,11 +53,12 @@ const renderSeries = (series) => {
 function renderFavorites(favorites) {
   let content = "";
   for (const favorite of favorites) {
+    const imageUrl = getImageUrl(favorite.images.jpg.large_image_url);
     content += `
         <div class="favorite-item" data-id="${favorite.mal_id}">
-            <img src="${favorite.images.jpg.large_image_url}">
-            <h3>${favorite.title}</h3>
-            <button class="remove-favorite" data-id="${favorite.mal_id}">X</button>
+          <img src="${imageUrl}">
+          <h3>${favorite.title}</h3>
+          <button class="remove-favorite" data-id="${favorite.mal_id}">X</button>
         </div>
     `;
   }
@@ -69,8 +72,11 @@ function renderFavorites(favorites) {
 
 function handleRemoveFavorite(event) {
   const seriesId = event.currentTarget.dataset.id;
-  favoritesList = favoritesList.filter((favorite) => favorite.mal_id !== parseInt(seriesId));
+  favoritesList = favoritesList.filter(
+    (favorite) => favorite.mal_id !== parseInt(seriesId)
+  );
   renderFavorites(favoritesList);
+  document.getElementById(seriesId).classList.remove('favorite');
   localStorage.setItem("favorites", JSON.stringify(favoritesList));
 }
 
@@ -86,7 +92,9 @@ const fetchSeriesData = () => {
     });
 };
 
-const favoritesDataFromLocalStorage = JSON.parse(localStorage.getItem("favorites"));
+const favoritesDataFromLocalStorage = JSON.parse(
+  localStorage.getItem("favorites")
+);
 if (favoritesDataFromLocalStorage !== null) {
   favoritesList = favoritesDataFromLocalStorage;
   renderFavorites(favoritesList);
@@ -112,4 +120,30 @@ function handleReset(event) {
 
   localStorage.removeItem("favorites");
   localStorage.removeItem("series");
+}
+
+function resetFavorites() {
+  favoritesList = [];
+  renderFavorites(favoritesList);
+  localStorage.removeItem("favorites");
+  const seriesItems = document.querySelectorAll(".series-item");
+  for (const seriesItem of seriesItems) {
+    seriesItem.classList.remove("favorite");
+  }
+}
+
+resetFavoritesButton.addEventListener("click", () => {
+  resetFavorites();
+});
+
+function getImageUrl(apiImageUrl) {
+  const missingImageUrl =
+    "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png";
+  const placeholderImageUrl =
+    "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
+  if (apiImageUrl === missingImageUrl) {
+    return placeholderImageUrl;
+  } else {
+    return apiImageUrl;
+  }
 }
